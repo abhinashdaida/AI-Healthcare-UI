75ba30828d93c444f12567a667500f9fba44a870
karthick10-qwe <karthickvarun007@gmail.com>
2026-08-17 17:42:55 +0530

    Header

--- src/pages/AdminPanel/Dashboard.jsx
+++ src/pages/AdminPanel/Dashboard.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Header from "@/shared/components/AdminPanel/Header";
 
 const Dashboard = () => {
   return (
     <div>
+      <Header />
       <h1>Dashboard</h1>
     </div>
   );
